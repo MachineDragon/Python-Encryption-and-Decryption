e3749cc5f7d2edef87cf036f84c4dca5607a2830
MichaelHajjar131 <58194115+MachineDragon@users.noreply.github.com>
2021-08-30 12:47:01 -0400

    Create Encryption_3.py

---
+++ Encryption_3.py
@@ -0,0 +1,18 @@
+alphabet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+user_input = input("Enter a secret message: ")
+
+encrypt = ""
+decrypt = ""
+
+for i in user_input:
+    place_holder = alphabet.index(i)
+    new_position = alphabet[place_holder + 5]
+    encrypt += new_position
+print(encrypt)
+
+for j in encrypt:
+    place_holder = alphabet.index(j)
+    old_position = alphabet[place_holder - 5]
+    decrypt += old_position
+print(decrypt)
