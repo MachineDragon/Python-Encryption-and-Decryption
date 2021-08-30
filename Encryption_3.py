a26ecf00d8361fb11c73742e8ef25452f1fa3e3b
MichaelHajjar131 <58194115+MachineDragon@users.noreply.github.com>
2021-08-30 12:52:59 -0400

    Update Encryption_3.py

--- Encryption_3.py
+++ Encryption_3.py
@@ -5,12 +5,14 @@ user_input = input("Enter a secret message: ")
 encrypt = ""
 decrypt = ""
 
+# To Encrypt
 for i in user_input:
     place_holder = alphabet.index(i)
     new_position = alphabet[place_holder + 5]
     encrypt += new_position
 print(encrypt)
 
+# To Decrypt
 for j in encrypt:
     place_holder = alphabet.index(j)
     old_position = alphabet[place_holder - 5]
