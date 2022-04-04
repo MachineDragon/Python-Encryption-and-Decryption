b78484ee2ebc89c5c55d9a16e94501433b9cc27a
MachineDragon <58194115+MachineDragon@users.noreply.github.com>
2022-04-04 11:15:43 -0400

    Update Encryption_3.py

--- Encryption_3.py
+++ Encryption_3.py
@@ -8,13 +8,11 @@ decrypt = ""
 # To Encrypt
 for i in user_input:
     place_holder = alphabet.index(i)
-    new_position = alphabet[place_holder + 5]
-    encrypt += new_position
+    encrypt += alphabet[place_holder + 5]
 print(encrypt)
 
 # To Decrypt
 for j in encrypt:
     place_holder = alphabet.index(j)
-    old_position = alphabet[place_holder - 5]
-    decrypt += old_position
+    decrypt += alphabet[place_holder - 5]
 print(decrypt)
